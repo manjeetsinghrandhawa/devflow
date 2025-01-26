7c63adcf89a2e66e93ed54a750f85c76c1025bc3
manjeetsinghh05 <138310696+manjeetsinghh05@users.noreply.github.com>
2025-01-26 18:40:19 +0530

    add api/webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,41 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Define protected routes
 const isProtectedRoute = createRouteMatcher(["/ask-question"]);
 
+// Define public routes
+const publicRoutes = [
+  "/",
+  "/app/api/webhook",
+  "question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "commiunity",
+  "/jobs",
+];
+
+const ignoredRoutes = ["/app/api/webhook", "/app/api/chatgpt"];
+
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+  // Bypass authentication for public routes
+  if (
+    publicRoutes.includes(req.nextUrl.pathname) &&
+    ignoredRoutes.includes(req.nextUrl.pathname)
+  ) {
+    return;
+  }
+
+  // Protect all other routes matched by `isProtectedRoute`
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
