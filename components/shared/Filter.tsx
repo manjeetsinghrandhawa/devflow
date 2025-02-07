afc4c4e85c83a5ba906d3e6eb4a782c5c6214df1
manjeetsinghh05 <138310696+manjeetsinghh05@users.noreply.github.com>
2025-02-07 14:39:25 +0530

    all_page_filtering_done

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -8,46 +8,59 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-// import { formUrlQuery } from '@/lib/utils';
+import { formUrlQuery } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
-  filters: { name: string; value: string }[];
+  filters: {
+    name: string;
+    value: string;
+  }[];
   otherClasses?: string;
   containerClasses?: string;
 }
 
 const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const paramFilter = searchParams.get("filter");
+
+  const handleUpdateParams = (value: string) => {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: "filter",
+      value,
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
+
   return (
     <div className={`relative ${containerClasses}`}>
       <Select
-      // onValueChange={(value) => handleUpdateParams(value)}
-      // defaultValue={paramsFilter || undefined}
+        onValueChange={handleUpdateParams}
+        defaultValue={paramFilter || undefined}
       >
         <SelectTrigger
-          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5 focus:outline-none`}
+          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
         >
           <div className="line-clamp-1 flex-1 text-left">
-            <SelectValue placeholder="Select a filter" />
+            <SelectValue placeholder="Select a Filter" />
           </div>
         </SelectTrigger>
-        <SelectContent className="text-dark500_light700 small-regular border-none bg-light-900 focus:outline-none active:outline-none dark:bg-dark-300">
+        <SelectContent>
           <SelectGroup>
-            {filters.map((item) => {
-              return (
-                <SelectItem
-                  className="cursor-pointer focus:bg-light-800 dark:focus:bg-dark-400"
-                  key={item.value}
-                  value={item.value}
-                >
-                  {item.name}
-                </SelectItem>
-              );
-            })}
+            {filters.map((item) => (
+              <SelectItem key={item.value} value={item.value}>
+                {item.name}
+              </SelectItem>
+            ))}
           </SelectGroup>
         </SelectContent>
       </Select>
     </div>
   );
 };
+
 export default Filter;
